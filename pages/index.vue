3c962c11e19f29105c0ff67442fa9c43ae716136
djackson29 <danika.c.jackson@gmail.com>
2022-03-19 12:20:42 -0700

    new project created

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    <h1>Box Office Bits</h1>
+    <h2>About</h2>
+    <nuxt-link to="/">Home</nuxt-link> |
+    <nuxt-link to="/comingSoon">Coming Soon</nuxt-link> |
+    <nuxt-link to="/discoverTitles">Discover New Titles</nuxt-link> |
+    <nuxt-link to="/motw">Movie of the Week</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homePage'
+}
+</script>
+
+<style lang="scss">
+
+</style>
