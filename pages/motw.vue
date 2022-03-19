a82a0ca1f3dca350a0ec5d0d199a51375901d4d5
djackson29 <danika.c.jackson@gmail.com>
2022-03-19 14:34:00 -0700

    working on API connection

--- pages/motw.vue
+++ pages/motw.vue
@@ -6,6 +6,7 @@
     <div class="row">
       <div class="col">
         <h2>Movie of the Week Archives</h2>
+        <pre>{{ info }}</pre>
       </div>
     </div>
 
@@ -13,20 +14,20 @@
     <div class="row">
 
       <!-- v-for to iterate through the placeholder array and populate each column with placeholder content -->
-        <div class="col-lg">
-          <div v-for="placeholder in placeholderContent" v-bind:key="placeholder.id">
+      <div class="col-lg">
+        <div v-for="placeholder in placeholderContent" v-bind:key="placeholder.id">
           <p>{{placeholder}}</p>
         </div>
       </div>
       <div class="col-lg">
         <div v-for="placeholder in placeholderContent" v-bind:key="placeholder.id">
-        <p>{{placeholder}}</p>
-      </div>
+          <p>{{placeholder}}</p>
+        </div>
       </div>
       <div class="col-lg">
         <div v-for="placeholder in placeholderContent" v-bind:key="placeholder.id">
-        <p>{{placeholder}}</p>
-      </div>
+          <p>{{placeholder}}</p>
+        </div>
       </div>
     </div>
   </main>
@@ -39,15 +40,60 @@ export default {
   name: 'movieWeek',
   data() {
     return {
-
+      info: null,
+      loading: true,
+      errored: false,
       // array for placeholder content
       placeholderContent: [
-          "<placeholder for API data>",
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        "<placeholder for API data>",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       ]
     }
   }
 }
+
+// import Vue from 'vue'
+// import axios from 'axios'
+
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response.data))
+      .catch(error => {
+        console.error(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+
+  }
+})
+
+
+
+// mounted () {
+//   axios
+//     .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+//     .then(response => (this.info = response))
+// }
+
+// fetch('http://www.omdbapi.com/?i=tt3896198&apikey=d36f6554')
+//   .then(data =>
+//   data.json()
+// )
+// .then(data => {
+//   console.log(data);
+//   document.querySelector('pre').innerHTML = JSON.stringify(data);
+// })
+// .catch(error => console.error(error));
 </script>
 
 <style lang="scss">
---
+++ plugins/axios.js
@@ -0,0 +1,9 @@
+
+
+export default function ({ $axios, redirect }) {
+  $axios.onError(error => {
+    if (error.response.status === 500) {
+      redirect('/sorry')
+    }
+  })
+}
